321eebe7a5ea7557c3b3246aeeef7128771f5572
Baron Hartono <baronhartono@gmail.com>
2019-07-29 10:40:52 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import axios from 'axios';
 
 class App extends Component {
   componentDidMount() {
-    axios.get('http://localhost:1997/products')
+    axios.get('http://localhost:1997/test')
       .then((res) => {
+        console.log('Masuk then')
         console.log(res.data)
+      }).catch((err) => {
+        console.log('terjadi error')
+        console.log(err)
+      })
+  }
+  onBtnAddClick = () => {
+    axios.post('http://localhost:1997/addproduct', {
+      id: 4,
+      nama: 'Popok Biskuat',
+      harga: 200000,
+      description: 'siapkah bayi anda menjadi macan!'
+    })
+      .then((res) => {
+          console.log(res.data)
       })
   }
 
@@ -13,6 +28,7 @@ class App extends Component {
     return (
       <div>
         <h1>Selamat datang di Popok Kece!</h1>
+        <input type="button" value="Add Product" onClick={this.onBtnAddClick} />
       </div>
     )
   }
