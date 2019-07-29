f9190f9a13ba1f1e4aa1217e8beef01e1052cf79
Baron Hartono <baronhartono@gmail.com>
2019-07-29 09:24:49 +0700

    commit 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    axios.get('http://localhost:1997/products')
+      .then((res) => {
+        console.log(res.data)
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Selamat datang di Popok Kece!</h1>
+      </div>
+    )
+  }
 }
 
 export default App;
